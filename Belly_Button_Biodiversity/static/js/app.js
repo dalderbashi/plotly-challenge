945189b30af6b7161236bddc8bcc9e79bba86186
wavyhair3 <diana.alderbashi@richmond.edu>
2019-11-02 22:59:17 -0400

    make it better

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -1,55 +1,109 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+    // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+    // Use `d3.json` to fetch the metadata for a sample
+    var url = "/metadata/" + sample;
+    d3.json(url).then((data) => {
 
-    // Use `.html("") to clear any existing metadata
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+        // Use d3 to select the panel with id of `#sample-metadata`
+        let metadataEl = d3.select("#sample-metadata");
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+        // Use `.html("") to clear any existing metadata
+        metadataEl.html("");
+
+        // Use `Object.entries` to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new
+        // tags for each key-value in the metadata.
+
+        Object.entries(data).forEach(([key, value]) => {
+            let row = metadataEl.append("div");
+            row.text(`${key}: ${value}`)
+
+
+        });
+
+        // BONUS: Build the Gauge Chart
+        // buildGauge(data.WFREQ);
+    });
 }
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    var url = "/samples/" + sample;
+    d3.json(url).then((data) => {
 
-    // @TODO: Build a Bubble Chart using the sample data
+        // @TODO: Build a Bubble Chart using the sample data
+        var trace1 = {
+            x: data.otu_ids,
+            y: data.sample_values,
+            text: data.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: data.sample_values,
+                color: data.otu_ids
+            }
+        };
+
+        var data = [trace1];
+
+        var layout = {
+            showlegend: false
+                //height: 600,
+                //width: 1400
+        };
+
+        Plotly.newPlot("bubble", data, layout, { responsive: true });
+    })
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+    var url = "/samples/" + sample;
+    d3.json(url).then((data) => {
+
+        var data = [{
+            values: data.sample_values.slice(0, 10),
+            labels: data.otu_ids.slice(0, 10),
+            hoverinfo: data.otu_labels.slice(0, 10),
+            type: 'pie'
+        }];
+
+        var layout = {
+            height: 700,
+            width: 900
+        };
+        Plotly.newPlot("pie", data, layout);
+    })
+
+};
 
 function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
+    // Use the list of sample names to populate the select options
+    d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+
+        // Use the first sample from the list to build the initial plots
+        const firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
     });
-
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
 }
 
 // Initialize the dashboard
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Plot.ly Homework - Belly Button Biodiversity
 
-![Bacteria by filterforge.com](Images/bacteria_by_filterforgedotcom.jpg)
+
 
 In this assignment, you will build an interactive dashboard to explore the [Belly Button Biodiversity DataSet](http://robdunnlab.com/projects/belly-button-biodiversity/).
 
